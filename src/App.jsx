b6f18f6fa5486f23ec01ff4944836b4c718b3a11
btaucher <btaucher88@gmail.com>
2024-11-08 00:44:05 -0300

    full commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+import * as S from "./style"
+import "./App.scss"
+
+
+export default function App(){
+  const [primeiroValor, setPrimeiroValor] = useState ()
+  const [segundoValor, setSegundoValor] = useState()
+  const [resultado, setResultado] = useState()
+  
+  const capturandoPrimeiroValor = (e) => {
+    setPrimeiroValor(Number(e.target.value))
+  }
+
+  const capturandoSegundoValor = (e) => {
+    setSegundoValor(Number(e.target.value))
+  }
+
+  const soma = () => {
+    setResultado(primeiroValor + segundoValor)
+  }
+
+  const subtracao = () => {
+    setResultado(primeiroValor - segundoValor)
+  }
+
+  const multiplicacao = () => {
+    setResultado(primeiroValor * segundoValor)
+  }
+
+  const divisao = () => {
+    setResultado(primeiroValor / segundoValor)
+  }
+
+  return(
+    <main>
+      <S.GlobalStyle/>
+      <h1>Calculadora de Gatinho</h1>
+      <input type="number" placeholder="Digite um número aqui" onChange={capturandoPrimeiroValor}/>
+      <input type="number" placeholder="Digite um número aqui" onChange={capturandoSegundoValor}/>
+      <div class="botoes">
+      <button onClick={soma}>+</button>
+      <button onClick={subtracao}>-</button>
+      <button onClick={multiplicacao}>*</button>
+      <button onClick={divisao}>/</button>
+      </div>
+      <h2>{resultado}</h2>
+    </main>
+  )
+}
---
+++ src/style.jsx
@@ -0,0 +1,7 @@
+import styled, {createGlobalStyle} from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0px;
+    padding: 0px;
+}`
